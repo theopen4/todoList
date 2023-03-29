bc9ae2667526baee75726e09e9ef252699dab921
theopen4 <kielze86@gmail.com>
2023-03-29 19:45:54 +0100

    grafic task

--- praticetodo/app.js
+++ praticetodo/app.js
@@ -1,4 +1,25 @@
-const wrapper = document.querySelector('#lastPosts');
-const loader = document.createElement('p');
-loader.innerText = 'chargements';
-wrapper.append(loader);
+ async function main (){
+    const wrapper = document.querySelector('#lastPosts');
+    const loader = document.createElement('p');
+    loader.innerText = 'chargements';
+    wrapper.append(loader);
+    try{
+    const r = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5',{
+       headers:{
+           Accept: 'application/json'
+       }
+    })
+    if(!r.ok){
+        throw new Error('erreur serveur');
+        
+
+    }
+} catch(e){
+    loader.innerText= 'impossible de charger les articles'
+        loader.style.color = 'red';
+        return
+
+}    
+
+}
+main()
