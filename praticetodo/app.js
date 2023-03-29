2847681745ff9110a93d1d2418d03d78f96a652d
theopen4 <kielze86@gmail.com>
2023-03-29 21:24:29 +0100

    grafic task

--- praticetodo/app.js
+++ praticetodo/app.js
@@ -1,3 +1,17 @@
+/**
+ * 
+ * @param {{title: string, body: string}} post 
+ * @returns {HTMLElement}
+ */ 
+ function createArticle(post){
+    const article = document.createElement('article');
+    article.innerHTML = `
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+          `
+        return article;  
+
+ }
  async function main (){
     const wrapper = document.querySelector('#lastPosts');
     const loader = document.createElement('p');
@@ -9,9 +23,16 @@
            Accept: 'application/json'
        }
     })
+    const posts = await r.json();
     if(!r.ok){
         throw new Error('erreur serveur');
         
+
+    }
+    
+    loader.remove()
+    for(let post of posts){
+        wrapper.append(createArticle(post));
 
     }
 } catch(e){
