1c9682f5c3507360afc93a583eab56dcdeeb39ec
theopen4 <kielze86@gmail.com>
2023-03-30 19:01:47 +0100

    create class todoList

--- todolist/todoList.js
+++ todolist/todoList.js
@@ -20,63 +20,61 @@ export class todoList{
             <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
             <button class="btn btn-outline-secondary" type="button" id="button-addon2">Ajouter</button>
         </div>    
-            <main>
+            <main class = "mainclass">
                 <div class="btn-group gap-2 d-md-block">
                     <button class="btn btn-primary" type="button">Toutes</button>
                     <button class="btn btn-primary" type="button">A faire</button>
                     <button class="btn btn-primary" type="button">Faites</button>
                   </div>
-                  <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                        <label class="form-check-label" for="flexCheckDefault">
-                            Default checkbox <span><button type="button" class="btn btn-outline-danger">Danger</button></span>  </li>
-                        </label>
-                        </div>
-                        <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-                        <label class="form-check-label" for="flexCheckChecked">
-                            Checked checkbox
-                        </label>
-                  </div>
             </main>
         `
+        const list = element.querySelector('.mainclass')
+            for(let todo of this.#todos){
+                const t = new todoListItem(todo)
+                t.appendTo(list)
+            }
     }
 }
 
 class todoListItem {
     /**@type {Todo} */
-    
+     #element
+     
     constructor(todo){
-        const id =` todo-${todo.id}`
+        const id =`todo-${todo.id}`
         const div = createElements('div', {
-            class : 'form-check'
+            class: 'form-check'
         })
         const checkbox = createElements('input',{
             class: 'form-check-input',
             type:  'checkbox',
+            value:"",
             id
         })
         
-        const label = document.createElements('label', {
+        const label = createElements('label', {
             class: 'form-check-label',
             for: id
         })
         label.innerText = todo.title
     
-        const button = document.createElements('button',{
+        const button = createElements('button',{
             class: 'btn btn-outline-danger',
         })
-        button.innerHTML = '<i class ="bi-trash"></i>'
+        button.innerHTML='<i class="bi-trash"></i>'
         div.append(checkbox)
         div.append(label)
         div.append(button)
+        this.#element = div
     }
     /**
      * 
      * @param {HTMLElement} element 
      */
     appendTo(element){
+        element.append(this.#element)
 
     }
 }
+
 
