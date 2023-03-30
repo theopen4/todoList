d0178d45f9502dfe07d6660aeabef70896102c42
theopen4 <kielze86@gmail.com>
2023-03-30 12:34:53 +0100

    init imput

--- todolist/app.js
+++ todolist/app.js
@@ -1,15 +1,18 @@
 import { fetchJson } from "./api.js";
-import { createElement } from "./dom.js";
+import { createElements } from "./dom.js";
 try {
-const todos = fetchJson('https://jsonplaceholder.typicode.com/posts?_limit=5')
+const todos = fetchJson('https://jsonplaceholder.typicode.com/posssts?_limit=5')
+console.log(todos);
 
-}catch(e){
-    const alertElement = createElement('div', {
-        class: 'alert alert-danger',
+}
+catch(e){
+    const alertElement = createElements('div', {
+        class: 'alert alert-danger m-2' ,
         role: 'alert'
     })
-    alertElement.innerText = "impossible de charger les elements"
-    document.body.append(alertElement)
+    alertElement.innerText = 'impossible de charger les elements'
+    document.body.prepend(alertElement)
+    console.log(alertElement)
 }
 
-console.log(todos);
+
--- todolist/dom.js
+++ todolist/dom.js
@@ -4,7 +4,7 @@
  * @return {HTMLElement}
  * 
  */
-export function createElement(tagName, attributes = {}){
+export function createElements(tagName, attributes = {}){
     const element = document.createElement(tagName);
     for (const [attribute, value] of Object.entries(attributes)){
         element.setAttribute(attribute, value);
